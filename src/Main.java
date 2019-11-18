2d0ae5baf831eb283a9061b227f6581bb58a7e90
suliangcai <suliangcai123@163.com>
2019-11-19 01:59:31 +0800

    add a login view

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import com.clm.view.Login;
 
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("wanghany123i");
+		Login login = new Login();
+		login.setVisible(true);
 	}
 }
