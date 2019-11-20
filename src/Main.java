7a105a9d6146ef67d3dac930640d887e2e933f7b
Dansheng <suliangcai123@163.com>
2019-11-21 01:03:14 +0800

    test again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import com.clm.View.SignIn;;
 
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("we can do anything");
+		System.out.println("we can do anything!");
 		SignIn login = new SignIn();
 		login.setVisible(true);
 	}
