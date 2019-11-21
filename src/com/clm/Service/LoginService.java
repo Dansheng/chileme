eddac6449976a1b1c65621f9c50bebeb452f8ab8
Dansheng <suliangcai123@163.com>
2019-11-21 19:53:16 +0800

    modified service

--- src/com/clm/Service/LoginService.java
+++ src/com/clm/Service/LoginService.java
@@ -1,5 +1,6 @@
 package com.clm.Service;
 
+import com.clm.Dao.WorkerDao;
 import com.clm.Model.Worker;
 
 public class LoginService {
@@ -12,6 +13,7 @@ public class LoginService {
 	}
 	public boolean login(Integer workId,String password) {
 		Worker worker = new Worker(workId,password);
+		WorkerDao workDao = new WorkerDao();
 		
 		return true;
 	}
