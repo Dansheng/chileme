3f4d8c62cfd1e75b4d8e3c66eead550c03b499a4
zhangshiyu <940868197@qq.com>
2019-11-21 21:43:53 +0800

    ui

--- src/com/clm/View/SignUp.java
+++ src/com/clm/View/SignUp.java
@@ -8,18 +8,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class SignUp extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
+	private JTextField textWorkId;
+	private JTextField textWorkName;
+	private JTextField textWorkPosition;
+	private JTextField textWorkTel;
+	private JTextField textWorkEmail;
+	private JTextField textWorkPassword;
+	private JTextField textWorkConfirmPassword;
 
 	/**
 	 * Launch the application.
@@ -41,9 +41,8 @@ public class SignUp extends JFrame {
 	 * Create the frame.
 	 */
 	public SignUp() {
-		setSize(800,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 350, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -53,77 +52,74 @@ public class SignUp extends JFrame {
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(170, 6, 130, 26);
-		panel.add(textField);
-		textField.setColumns(10);
+		textWorkId = new JTextField();
+		textWorkId.setBounds(170, 6, 130, 26);
+		panel.add(textWorkId);
+		textWorkId.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(170, 44, 130, 26);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		textWorkName = new JTextField();
+		textWorkName.setBounds(170, 58, 130, 26);
+		panel.add(textWorkName);
+		textWorkName.setColumns(10);
 		
-		JLabel label = new JLabel("员工编号");
-		label.setBounds(112, 11, 61, 16);
-		panel.add(label);
+		textWorkPosition = new JTextField();
+		textWorkPosition.setBounds(170, 91, 130, 26);
+		panel.add(textWorkPosition);
+		textWorkPosition.setColumns(10);
 		
-		JLabel label_1 = new JLabel("姓名");
-		label_1.setBounds(112, 49, 61, 16);
-		panel.add(label_1);
+		textWorkTel = new JTextField();
+		textWorkTel.setBounds(170, 137, 130, 26);
+		panel.add(textWorkTel);
+		textWorkTel.setColumns(10);
 		
-		JLabel label_2 = new JLabel("职位");
-		label_2.setBounds(112, 82, 61, 16);
-		panel.add(label_2);
+		textWorkEmail = new JTextField();
+		textWorkEmail.setBounds(170, 165, 130, 26);
+		panel.add(textWorkEmail);
+		textWorkEmail.setColumns(10);
 		
-		JLabel label_3 = new JLabel("电话");
-		label_3.setBounds(112, 110, 61, 16);
-		panel.add(label_3);
+		textWorkPassword = new JTextField();
+		textWorkPassword.setBounds(170, 231, 130, 26);
+		panel.add(textWorkPassword);
+		textWorkPassword.setColumns(10);
 		
-		JLabel label_4 = new JLabel("邮箱");
-		label_4.setBounds(112, 138, 61, 16);
-		panel.add(label_4);
+		textWorkConfirmPassword = new JTextField();
+		textWorkConfirmPassword.setBounds(170, 275, 130, 26);
+		panel.add(textWorkConfirmPassword);
+		textWorkConfirmPassword.setColumns(10);
 		
-		JLabel label_5 = new JLabel("工资卡号");
-		label_5.setBounds(112, 166, 61, 16);
-		panel.add(label_5);
+		JLabel labelWorkId = new JLabel("员工编号");
+		labelWorkId.setBounds(74, 11, 61, 16);
+		panel.add(labelWorkId);
 		
-		JLabel label_6 = new JLabel("密码");
-		label_6.setBounds(112, 194, 61, 16);
-		panel.add(label_6);
+		JLabel labelWorkName = new JLabel("姓名");
+		labelWorkName.setBounds(74, 63, 61, 16);
+		panel.add(labelWorkName);
 		
-		JLabel label_7 = new JLabel("确认密码");
-		label_7.setBounds(112, 222, 61, 16);
-		panel.add(label_7);
+		JLabel labelWorkPosition = new JLabel("职位");
+		labelWorkPosition.setBounds(74, 96, 61, 16);
+		panel.add(labelWorkPosition);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(170, 77, 130, 26);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
+		JLabel labelWorkTel = new JLabel("电话");
+		labelWorkTel.setBounds(74, 142, 61, 16);
+		panel.add(labelWorkTel);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(170, 110, 130, 26);
-		panel.add(textField_3);
-		textField_3.setColumns(10);
+		JLabel labelWorkEmail = new JLabel("邮箱");
+		labelWorkEmail.setBounds(74, 170, 61, 16);
+		panel.add(labelWorkEmail);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(170, 138, 130, 26);
-		panel.add(textField_4);
-		textField_4.setColumns(10);
+		JLabel labelWorkPassword = new JLabel("密码");
+		labelWorkPassword.setBounds(74, 236, 61, 16);
+		panel.add(labelWorkPassword);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(170, 161, 130, 26);
-		panel.add(textField_5);
-		textField_5.setColumns(10);
+		JLabel labelWorkConfirmPassword = new JLabel("确认密码");
+		labelWorkConfirmPassword.setBounds(74, 280, 61, 16);
+		panel.add(labelWorkConfirmPassword);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(170, 189, 130, 26);
-		panel.add(textField_6);
-		textField_6.setColumns(10);
 		
-		textField_7 = new JTextField();
-		textField_7.setBounds(170, 222, 130, 26);
-		panel.add(textField_7);
-		textField_7.setColumns(10);
+		
+		JButton buttonSignUp = new JButton("注册");
+		buttonSignUp.setBounds(103, 357, 117, 29);
+		panel.add(buttonSignUp);
 	}
 
 }
