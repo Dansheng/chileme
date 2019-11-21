8479c4e2fff0837bd7fbac72238ac0bbb93c8dba
Dansheng <suliangcai123@163.com>
2019-11-21 20:09:13 +0800

    controller

--- src/com/clm/Controller/Controller.java
+++ src/com/clm/Controller/Controller.java
@@ -1,5 +1,18 @@
 package com.clm.Controller;
 
-public class Controller {
+import com.clm.Service.LoginService;
 
+public class Controller {
+	private static Controller controller = new Controller();
+	private Controller() {
+		
+	}
+	public static Controller getInstance() {
+		return controller;
+	}
+	// for login 
+	public void login(Integer workId,String password) {
+		LoginService login = LoginService.getInstance();
+		login.login(workId, password);
+	}
 }
