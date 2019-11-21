10ba5c9be77a3a2a6c6d8e3a7c5d1a0ed552c041
Dansheng <suliangcai123@163.com>
2019-11-21 19:25:16 +0800

    add Card

--- src/com/clm/Model/Card.java
+++ src/com/clm/Model/Card.java
@@ -1,5 +1,44 @@
 package com.clm.Model;
 
 public class Card {
-	//BankCard
+	Integer WorkerId;
+	String cardNumber;
+	Double balance;
+	Card(){
+		balance = 0.00;
+	}
+	// get 
+	public Integer getWorkerId() {
+		return WorkerId;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	// set
+	public boolean setWorkerId() {
+		return true;
+	}
+	public boolean setCardNumber() {
+		return true;
+	}
+	public boolean setBalance() {
+		return true;
+	}
+	// saveMoney
+	public boolean deposit(double money) {
+		try {
+			balance += money;
+		}
+		catch(Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	
 }
