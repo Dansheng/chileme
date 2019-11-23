3a77885da341ea689917bceb4ce0fb7eecf1cb9a
zhangshiyu <940868197@qq.com>
2019-11-23 21:08:17 +0800

    ui

--- src/com/clm/View/SignUp.java
+++ src/com/clm/View/SignUp.java
@@ -47,7 +47,7 @@ public class SignUp extends JFrame implements ActionListener{
 	 * Create the frame.
 	 */
 	public SignUp() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 350, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
