b516199b153391b7bfd668e1c692bd3ac7d78a09
wanghanyi820 <2576954039@qq.com>
2019-11-28 19:51:00 +0800

    add four methods

--- src/com/clm/Dao/OrderDao.java
+++ src/com/clm/Dao/OrderDao.java
@@ -3,6 +3,8 @@ package com.clm.Dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.clm.Model.Order;
 import com.clm.Model.OrderInfo;
@@ -36,60 +38,108 @@ public class OrderDao {
 	}
 	public Order QueryOrder(Integer OrderId)
 	{
-		Order order=new Order();
+		OrderInfo orderinfo=new OrderInfo();
+		Order order=new Order(orderinfo);
 		try
 		{
 			Statement stmt=JdbcUnit.getConnection().createStatement();
-			String query1="SELECT * FROM orderW WHERE orderId="+OrderId;
+			String query1="SELECT* FROM orderInfo WHERE orderId="+OrderId;
+			System.out.println(query1);
 			ResultSet rs=stmt.executeQuery(query1);
+			while(rs.next())
+			{
+				orderinfo.setOrderId(rs.getInt("orderId"));
+				orderinfo.getDish_count().put(rs.getInt("dishId"),rs.getInt("count"));
+				orderinfo.setWordDescription(rs.getString("wordDescription"));
+				orderinfo.setStarDesciption(rs.getInt("starDescription"));
+			}
+			String query2="SELECT* FROM orderW WHERE orderId="+OrderId;
+			rs=stmt.executeQuery(query2);
 			while(rs.next())
 			{
 				order.setOrderId(rs.getInt("orderId"));
 				order.setWorkId(rs.getInt("workId"));
 				order.setServeTime(rs.getString("serveTime"));
 				order.setServeAddr(rs.getString("serveAdd"));
-				order.setPayWay(rs.getString("payWay"));
 				order.setPayState(rs.getString("payState"));
+				order.setPayWay(rs.getString("payWay"));
 				order.setWordDescription(rs.getString("wordDescription"));
 				order.setStarDescription(rs.getInt("starDescription"));
 			}
-//			String query="SELECT * FROM orderInfo WHERE orderId="+OrderId;
-//			System.out.println(query);
-//			
-//			ResultSet r=stmt.executeQuery(query);
-//			System.out.println(r.getInt("orderId"));
-//			
-//			System.out.println(r.first());
-//			while(r.next())
-//			{
-//				order.getOrderInfo().getDish_count().put(r.getInt("dishId"), r.getInt("count"));
-//			}
-//			System.out.println(order);
+			//System.out.println(order);
 		}catch(SQLException e)
 		{
 			e.printStackTrace();
 		}
 		return order;
 	}
+	public boolean setOrderState(Order order)
+	{
+		Integer s=null;
+		try
+		{
+			Statement stmt=JdbcUnit.getConnection().createStatement();
+			String set="UPDATE orderW SET payState='"+order.getPayState()+"' WHERE orderId="+order.getOrderId();
+			s=stmt.executeUpdate(set);
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		if(s!=0)
+		{
+			return true;
+		}
+		else return false;
+	}
+	public ArrayList<Order> getOrderList(int WorkerId)
+	{
+		ArrayList<Order> orderlist=new ArrayList<Order>();
+		try
+		{
+			Statement stmt=JdbcUnit.getConnection().createStatement();
+			String query="SELECT* FROM orderW WHERE workId="+WorkerId;
+			ResultSet rs=stmt.executeQuery(query);
+			while(rs.next())
+			{
+				orderlist.add(QueryOrder(rs.getInt("orderId")));
+			}
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return orderlist;
+	}
 	public static void main(String[] args)
 	{
-//		OrderInfo orderinfo=new OrderInfo();
-//		orderinfo.setOrderId(1);
-//		orderinfo.getDish_count().put(1, 1);
-//		orderinfo.setWordDescription("kaka");
-//		orderinfo.setStarDesciption(2);
-//		Order order=new Order(orderinfo);
-//		order.setOrderId(1);
-//		order.setWorkId(1);
-//		order.setServeTime("2019年8月20");
-//		order.setServeAddr("海棠");
-//		order.setPayWay("cash");
-//		order.setPayState("finished");
-//		order.setWordDescription("good");
-//		order.setStarDescription(1);
-//		OrderDao orderdao=new OrderDao();
-////		System.out.println(orderdao.AddOrder(order));
+		OrderInfo orderinfo=new OrderInfo();
+		orderinfo.setOrderId(1);
+		orderinfo.getDish_count().put(1, 1);
+		orderinfo.setWordDescription("kaka");
+		orderinfo.setStarDesciption(2);
+		Order order=new Order(orderinfo);
+		order.setOrderId(1);
+		order.setWorkId(1);
+		order.setServeTime("2019年8月20");
+		order.setServeAddr("海棠");
+		order.setPayWay("cash");
+		order.setPayState("preparing");
+		order.setWordDescription("good");
+		order.setStarDescription(1);
+		Order order2=new Order(orderinfo);
+		order2.setOrderId(2);
+		order2.setWorkId(1);
+		order2.setServeTime("2019年11月28");
+		order2.setServeAddr("海棠");
+		order2.setPayWay("cash");
+		order2.setPayState("finished");
+		order2.setWordDescription("good");
+		order2.setStarDescription(1);
+		OrderDao orderdao=new OrderDao();
+//		System.out.println(orderdao.AddOrder(order2));
 //		System.out.println(orderdao.QueryOrder(1));
+//		System.out.println(orderdao.setOrderState(order));
+		System.out.println(orderdao.getOrderList(1));
+		
 		
 	}
 }
